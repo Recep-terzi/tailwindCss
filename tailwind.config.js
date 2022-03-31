6a4c4f1058d24f9e67ce30ee3f8d92919f1ee576
Recep Terzi <recepterzi-67@hotmail.com>
2022-03-31 22:50:21 +0300

    tailwindCss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/**/*.html'],
+  theme: {
+    extend: {
+      colors :{
+        twitter : '#1da1f2'
+      },
+      spacing:{
+        '15':'3.75rem'
+      },
+      
+    },
+  },
+  plugins: [],
+}
